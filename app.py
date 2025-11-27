661261f0c29c69d7ba1d3731b446de7157971815
Kiril Chervoniak <42374203+kirilche@users.noreply.github.com>
2025-11-27 12:17:17 +0200

    form name + default

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
 default_name = os.environ.get('DEFAULT_USERNAME')
@@ -8,5 +8,21 @@ if not default_name:
     default_name = "Світ!"
 
 @app.route('/')
-def hello_world():
-    return f'Привіт, {default_name}'
+def home():
+    return render_template('login.html')
+
+@app.route('/success/<name>')
+def success(name):
+    return f'Привіт {name}\n Вітання {default_name}' 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+if __name__ == '__main__':
+    app.run(debug=True)
