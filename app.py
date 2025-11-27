151d0969dc7fd1bab4f2b140d96f8d9b867daa69
Kiril Chervoniak <42374203+kirilche@users.noreply.github.com>
2025-11-27 11:58:53 +0200

    add default name

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
+default_name = os.environ.get('DEFAULT_USERNAME')
+
+if not default_name:
+    default_name = "Світ!"
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'Привіт, {default_name}'
